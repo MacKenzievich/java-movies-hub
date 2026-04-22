cd5d95645d6154fa588c32ca5aa58c34aec1aa83
mackenzievich <mackenzievich@gmai.com>
2026-04-22 20:30:53 +0300

    finish

--- src/test/ru/practicum/moviehub/http/MoviesApiTest.java
+++ src/test/ru/practicum/moviehub/http/MoviesApiTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.moviehub.http;
 
-import com.google.gson.Gson;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
